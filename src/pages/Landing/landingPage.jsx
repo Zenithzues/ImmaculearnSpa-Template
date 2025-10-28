bd91e99244dfaa5c0721da346dfc9a6aab2c16aa
wilson <wilsonesmabe2003@gmail.com>
2025-10-28 21:42:20 +0800

    add initial folder structure and create custom hooks

--- src/pages/Landing/landingPage.jsx
+++ src/pages/Landing/landingPage.jsx
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
 import useSocket from "@/hooks/useSocket.js";
 import ChatList from "../User_chats/user_chats"; // import Sidebar/MainPage
+import LandingPage2 from "./landingPage2";
+import TopFeatures from "./topFeatures";
+import { encryptData, decryptData } from "../../hooks/useLocalStorage";
 
 const LandingPage = () => {
   const [showMainPage, setShowMainPage] = useState(false);
+
+  // app = useLocalStorage("sample", {"name": "wilson"}, "secrets")
+  
+  // encryptData({"sample":"hello world"}, "secrets")
+
+  const run = async () => {
+    const encrypted = await encryptData({ hello: "world" });
+    const decrypted = await decryptData(encrypted);
+    console.log({ encrypted, decrypted });
+  };
+
+  run();
 
   return (
     <div className="max-w-full font-sans bg-gradient-to-r from-white to-[#fef6ff] min-h-screen relative overflow-hidden">
@@ -29,33 +44,43 @@ const LandingPage = () => {
       <main className="flex flex-col md:flex-row items-center justify-between px-8 lg:px-20 mt-10">
         {/* Left Content */}
         <div className="max-w-lg">
-          <h1 className="text-4xl font-bold leading-snug">
+          <h1 className="text-[3rem] font-bold leading-snug">
             <span className="text-green-600">Wherever</span> you are, <br />
-            <span className="text-green-600">Learn Together!</span>
+            <span className="p-16 text-green-600">Learn Together!</span>
           </h1>
 
           {/* Sign Up Button */}
-          <button className="mt-6 px-6 py-2 bg-white border border-black rounded-full shadow-md hover:bg-gray-100 flex items-center gap-2">
+          {/* <button className="mt-6 px-6 py-2 bg-white border border-black rounded-full shadow-md hover:bg-gray-100 flex items-center gap-2">
             Sign Up <span className="text-green-600 font-bold">→</span>
-          </button>
+          </button> */}
 
           {/* Dashboard Trigger */}
-          <button
+          {/* <button
             onClick={() => setShowMainPage(true)}
             className="mt-6 px-6 py-3 bg-indigo-700 text-white rounded-lg shadow-md hover:bg-indigo-800"
           >
             Go to Dashboard
-          </button>
+          </button> */}
 
-          <p className="mt-6 text-gray-800">
-            ImmacuLearn transforms the way you experience education by offering
-            a seamless, engaging, and collaborative platform.
+          <p className="mt-6 text-gray-800 font-bold">
+            ImmacuLearn transforms the way you experience education by offering a seamless, engaging, and collaborative platform designed for both students and educators.
           </p>
+          <div className="relative inline-block mt-6">
+            <button
+              onClick={() => setShowMainPage(true)}
+              className="relative ml-2 z-10 px-6 py-3 bg-indigo-700 text-white rounded-lg shadow-md
+                    border-[3px] border-black shadow-[6px_6px_0_0_#000]
+                    "
+            >
+              Explore Now
+            </button>
+            
+          </div>
         </div>
 
         {/* Right Image */} 
         <div className="mt-10 md:mt-0 relative z-10"> 
-          <img src="./src/assets/heroes.png" alt="Students Learning" className="w-[450px] max-w-full" /> 
+          <img src="./src/assets/heroes.png" alt="Students Learning" className="w-[600px] max-w-full" /> 
         </div> 
         <div className="absolute right-0 z-0"> 
           <img src="./src/assets/Blob.png" alt="" className="w-[500px] max-w-full" /> 
@@ -68,7 +93,8 @@ const LandingPage = () => {
       {/* Popups */}
       {showMainPage && (
         <div className="fixed inset-0 z-50 bg-white">
-          <ChatList />
+          {/* <LandingPage2 /> */}
+          <TopFeatures />
         </div>
       )}
     </div>
